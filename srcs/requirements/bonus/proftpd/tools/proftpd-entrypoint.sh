6c5125f51657c972f8211f01562b5a21a38da025
stacks-of-data <coder64bit@gmail.com>
2025-07-11 22:25:17 +0300

    Permissions fix

--- srcs/requirements/bonus/proftpd/tools/proftpd-entrypoint.sh
+++ srcs/requirements/bonus/proftpd/tools/proftpd-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+chown -R "$PROFTPD_LOGIN0_USERNAME":"$PROFTPD_GROUPNAME" /var/www/wordpress
+chmod -R 770 /var/www/wordpress
+
 LOGIN0_PASS=$(< /run/secrets/proftpd_login0_pass tr -d '\n')
 
 echo "$PROFTPD_LOGIN0_USERNAME:$LOGIN0_PASS" | chpasswd > /dev/null 2>&1
